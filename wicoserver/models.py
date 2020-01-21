6336c0c1e1ce62d90d20b9b85ecdb250377a704b
David Bauer <mail@david-bauer.net>
2020-01-21 23:11:35 +0100

    models: update models

--- wicoserver/models.py
+++ wicoserver/models.py
@@ -7,6 +7,8 @@ class Site(models.Model):
     # Encapsulation for management traffic
     mgmt_vxlan = models.CharField(null=True, max_length=128)
     mgmt_vlan = models.IntegerField(null=True)
+    country = models.CharField(max_length=2)
+    legacy_rates = models.BooleanField(default=True)
 
 
 class Location(models.Model):
@@ -17,13 +19,24 @@ class Location(models.Model):
 class AccessPoint(models.Model):
     name = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    location = models.ForeignKey(to=Location, on_delete=models.SET_NULL)
-    site = models.ForeignKey(to=Site, on_delete=models.SET_NULL)
+    location = models.ForeignKey(to=Location, on_delete=models.SET_NULL, null=True)
+    site = models.ForeignKey(to=Site, on_delete=models.SET_NULL, null=True)
     uplink_port = models.CharField(max_length=16)
 
 
 class Radio(models.Model):
-    access_point = models.ForeignKey(to=AccessPoint, on_delete=models.SET_NULL)
+    class HTModes(models.TextChoices):
+        HT20 = 'H2', gettext_lazy('HT20')
+        HT40 = 'H4', gettext_lazy('HT40')
+        VHT20 = 'V2', gettext_lazy('VHT20')
+        VHT40 = 'V4', gettext_lazy('VHT40')
+        VHT80 = 'V8', gettext_lazy('VHT80')
+
+    access_point = models.ForeignKey(to=AccessPoint, on_delete=models.CASCADE)
+    radio_idx = models.IntegerField()
+    htmode = models.CharField(max_length=2, choices=HTModes.choices)
+    channel = models.IntegerField()
+    path = models.CharField(max_length=32)
     min_channel = models.IntegerField()
     max_channel = models.IntegerField()
 
@@ -31,6 +44,7 @@ class Radio(models.Model):
 class SSID(models.Model):
     class EncryptionMethods(models.TextChoices):
         WPA2_PSK = 'P2', gettext_lazy('psk2')
+        WPA3_PSK = 'P3', gettext_lazy('psk3')
 
     name = models.CharField(max_length=32)
     vxlan = models.CharField(null=True, max_length=128)
