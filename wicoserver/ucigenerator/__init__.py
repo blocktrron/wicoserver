8b03001f2150a877714898a7dc8b98a94a7731b5
David Bauer <mail@david-bauer.net>
2020-01-25 12:31:45 +0100

    ucigenerator: fix UCI file generation

--- wicoserver/ucigenerator/__init__.py
+++ wicoserver/ucigenerator/__init__.py
@@ -1,11 +1,12 @@
 def generate_uci_file(settings: dict):
-    output = str()
+    output = ""
     for section in settings["sections"]:
-        output + "config {config_type} '{name}'".format(config_type=section["_type"], name=section["_name"])
-        for name, value in section["options"]:
+        output += "config {config_type} '{name}'\n".format(config_type=section["_type"], name=section["_name"])
+        for name, value in section["options"].items():
             if type(value) == list:
                 for element in value:
-                    output + "\tlist {name} '{value}'".format(name=name, value=element)
+                    output += "\tlist {name} '{value}'\n".format(name=name, value=element)
             else:
-                output + "\toption {name} '{value}'".format(name=name, value=value)
+                output += "\toption {name} '{value}'\n".format(name=name, value=value)
+        output += "\n"
     return output
